042a576c370c89530fb1b739b48fb79e391cb420
julieva cohen sors <julieva@MacBook-Pro-10.local>
2020-10-10 13:02:31 +0200

    checkbox

--- app.R
+++ app.R
@@ -1,67 +1,251 @@
 ##Exercice : Shiny Deaths from Covid-19
-#Load Library
+
+#resoudre le pb de selection sous variable (reactiv) : Antoine
+#rajouter region : Antoine
+
+#Amelioration du ggplot : Alexandra et Julieva
+#Mettre menu selection sur le haut
+#Comparaison des graphiques : rajouter 
+#plusieurs courbes en fonction de la selection utilisateur
+#changement du nom de laxe des ordonnees en 
+#fonction de la metric 
+
+#rajouter deuxieme onglet : simon 
+#map
+
+
+#library
 library(shiny)
 library(magrittr)
 library(data.table)
 library(ggplot2)
 library(readr)
+library(httr)
+library(readxl)
+library(stringr)
+library(shinyWidgets)
 
+#global_scope
+selected_country <- unique(PlotDT$Country_Name)
+selected_topic <- unique(goalD$Topic)
+selected_subtopic_1 <- list()
+selected_subtopic_2 <- list()
+selected_subtopic_3 <- list()
+selected_indicator <- list()
 
-# import data
-urlfile="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-coronaD<-urlfile%>% url %>% read_csv %>% as.data.table 
-# Function
-Dates<-colnames(coronaD[,-c(1:4)])
-setDT(coronaD)
-PlotDT = melt(coronaD, id.vars = 1:4, measure.vars = Dates, variable.name = "Date",value.name = "Dead")
-PlotDT$Date = PlotDT$Date %>% as.Date(format = "%m/%d/%y")
-SelectedCountries = c("France","Italy","Spain","Germany")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
-    # Give the page a title
-    titlePanel("Death from Covid"),
-
-    # Generate a row with a sidebar
-    sidebarLayout(      
-        
-        # Define the sidebar with one input
-        sidebarPanel(
-            selectInput("countryregion", "Country/Region:", 
-                        choices=SelectedCountries),
-            hr(),
-            helpText("Data from CSSEGIS and Data JH")
-        ),
-        
-        # Create a spot for the barplot
-        mainPanel(
-            plotOutput("countryPlot")  
-        )
-        
+  titlePanel(
+    # app title/description
+    h1("MGD"),
+  ),
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Here you can find some graphical information
+               about World Development Goals"),
+      br(), 
+      helpText("First, choose the World Development Indicators."),
+      br(), 
+      
+      # inputs
+      selectInput("topic", 
+                  h2("Choose a topic", align = "center"),
+                  selected_topic),
+      
+      br(), 
+      selectInput("subtopic_1", 
+                  h2("Choose a subtopic 1", align = "center"),
+                  selected_subtopic_1, 
+                  choices = NULL),
+      
+      br(), 
+      selectInput("subtopic_2", 
+                  h2("Choose a subtopic 2", align = "center"),
+                  selected_subtopic_2, 
+                  choices = NULL),
+      
+      br(), 
+      selectInput("subtopic_3", 
+                  h2("Choose a subtopic 3", align = "center"),
+                  selected_subtopic_3, 
+                  choices = NULL),
+      
+      br(), 
+      selectInput("indicator", 
+                  h2("Choose a indicator", align = "center"),
+                  selected_indicator, 
+                  choices = NULL),
+      
+      br(), 
+      
+      checkboxGroupInput("country", 
+                  h2("Choose a country", align = "center"),
+                  selected_country, 
+                  "France"),
+      br(),
+      radioButtons("y_axis_choice", 
+                   h2("Axis :", align = "center"), 
+                   c("linear", "logarithmic")), 
+      
+      br(), 
+      dateRangeInput("date_choice", 
+                     h2("Choose a date range :", align="center"),
+                     format = "yyyy",
+                     start="1972"),
+      
+      br(),
+      p(strong("Full data is available just below."), style="strong"),
+      br(),
+      a(strong("DATA AVAILABLE HERE"), href="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"),
+      br(),
+      img(src="https://i1.wp.com/www.un.org/sustainabledevelopment/wp-content/uploads/2015/12/english_SDG_17goals_poster_all_languages_with_UN_emblem_1.png?fit=728%2C451&ssl=1", height = 72, width = 72, style="margin-left:80px"),
+      br(), 
+      
+      ),
+    mainPanel(
+      # outputs
+      plotOutput("displot"), 
+      
+      plotOutput("summary"),
+      
     )
+  )
 )
 
 
 #  Define a server for the Shiny app
-server <- function(input, output) {
-    # Fill in the spot we created for a plot
-    output$countryPlot <- renderPlot({
-        # Render the plot
+server <- function(input, output, session) {
+  
+  #remise a zero de l'indicator
+  
+  observeEvent(input$topic, {
+    #find indicator list
+    updateSelectInput(
+      session,
+      inputId = "indicator",
+      choices = ''
+    )
+    #
+    
+    
+    
+    #look if there is something in SubTopic1
+    choices <- unique(goalD[goalD$Topic == input$topic, list(SubTopic1)])
+    
+    #change the value of subtopic_1 in function of the value of topic  
+    updateSelectInput(
+      session,
+      inputId = "subtopic_1",
+      choices = choices
+    )
+  })
+  
+  observeEvent(input$subtopic_1, {
+    #look if there is something in SubTopic2
+    choices <- unique(goalD[goalD$SubTopic1 == input$subtopic_1, list(SubTopic2)])
+    
+    not_value_subtopic_2 = as.vector(is.na(choices[1]))
+    
+    #change the value of subtopic_2 in function of the value of topic
+    if(not_value_subtopic_2) {
+      updateSelectInput(
+        session,
+        inputId = "subtopic_2",
+        choices = ''
+      )
       
+      updateSelectInput(
+        session,
+        inputId = "subtopic_3",
+        choices = ''
+      )
+      
+      #find indicator list
+      updateSelectInput(
+        session,
+        inputId = "indicator",
+        choices = unique(goalD[goalD$SubTopic1 == input$subtopic_1, list(Series_Name.x)])
+      )
+      #
+      
+    }
+    else {
+      updateSelectInput(
+        session,
+        inputId = "subtopic_2",
+        choices = choices
+      )
+    }
+    
+  })
+  
+  observeEvent(input$subtopic_2, {
+    if (input$subtopic_2 != '') {
+      #look if there is something in SubTopic3
+      choices <- unique(goalD[goalD$SubTopic2 == input$subtopic_2, list(SubTopic3)])
+      
+      not_value_subtopic_3 = as.vector(is.na(choices[1]))
+      
+      #change the value of subtopic_2 in function of the value of topic
+      if(not_value_subtopic_3) {
+        print("bonjour")
+        updateSelectInput(
+          session,
+          inputId = "subtopic_3",
+          choices = ''
+        )
         
-PlotDT[`Country/Region` %in% SelectedCountries & is.na(`Province/State`)] %>% ggplot(aes(x = Date, y = Dead)) +
-            geom_line(data = PlotDT[`Country/Region` == "France" & is.na(`Province/State`)])+
-            geom_line(data = PlotDT[`Country/Region` == "Germany"])+
-            geom_line(data = PlotDT[`Country/Region` == "Italy"])+
-            geom_line(data = PlotDT[`Country/Region` == "Spain"])+
-            geom_line(data = PlotDT[`Country/Region` == input$countryregion & is.na(`Province/State`)], color = 'red')
+        #find indicator list
+        updateSelectInput(
+          session,
+          inputId = "indicator",
+          choices = unique(goalD[goalD$SubTopic2 == input$subtopic_2, list(Series_Name.x)])
+        )
+        #
         
-        
-        
-        
-    })
+      }
+      else {
+        print("au revoir")
+        updateSelectInput(
+          session,
+          inputId = "subtopic_3",
+          choices = choices
+        )
+      }
+    }
+    
+  })
+  
+  observeEvent(input$subtopic_3, {
+    if (input$subtopic_3 != '') {
+      #find indicator list
+      updateSelectInput(
+        session,
+        inputId = "indicator",
+        choices = unique(goalD[goalD$SubTopic3 == input$subtopic_3, list(Series_Name.x)])
+      )
+      #
+    }
+  })
+  
+  
+  
+  ?checkboxGroupInput()
+  
+  output$displot <- renderPlot({
+    
+    p <- switch(input$y_axis_choice,"linear" = NULL,"logarithmic"=scale_y_log10())
+    
+    ggplot(PlotDT[`Country_Name`==input$country &`Series_Name.x`==input$indicator],
+           aes(x= Date, y = Value)) + 
+      geom_line() + scale_x_date(limits = input$date_choice) + p
+    
+  })
+  
 }
+
+
 
 
 # Run the application 
