e8fab5985dde11914f51133723257ebf87fd7270
julieva cohen sors <julieva@macbook-pro-10.home>
2020-10-11 18:28:01 +0200

    barchart

--- app.R
+++ app.R
@@ -310,25 +310,23 @@ server <- function(input, output, session) {
   
   
   output$displot2 <- renderPlotly({
-    
     p <- switch(input$y_axis_choice,"linear" = NULL,"logarithmic"=scale_y_log10())
     
     Plot_choices <- switch(input$aggregation,"Region"=PlotDT_Region,
                            "Income_group"=PlotDT_Income_group,
                            "Lending_Category"=PlotDT_Lending_category, 
                            "Other"=PlotDT_Other)
-
     
-    q<-boxplot() +
-      geom_bar(data=PlotDT[`Country_Name`==input$country &`Series_Name.x`==input$indicator], 
-                aes(x=Date, y=Value,colour=input$country))+ 
-      geom_bar(data=Plot_choices[`Series_Name.x`==input$indicator], 
-                aes(x= Date, y = Value, 
-                    colour = Region)) + p
+     q <- ggplot() + geom_bar(data=PlotDT[`Country_Name`=="France" 
+                                         &`Series_Name.x`=="Agricultural machinery, tractors"], 
+                             aes(x=Date,y=Value, colour="France"), stat="identity")+
+      geom_bar(data=Plot_choices[`Series_Name.x`=="Agricultural machinery, tractors"], 
+               aes(x=Date,y=Value, colour=Region), stat="identity")
+    q
     
-    ggplotly(q)
     
   })
+  
   
   output$displot3 <- renderPlot({
     WorldData <- map_data('world') %>% filter(region != "Antarctica") %>% fortify
